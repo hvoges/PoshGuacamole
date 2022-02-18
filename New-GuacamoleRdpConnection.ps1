a5c26747379296e3433365718087451d33eeecc2
Holger Voges <holger.voges@netz-weise.de>
2022-02-18 21:09:04 +0100

    switched to pure json in a Here-string

--- New-GuacamoleRdpConnection.ps1
+++ New-GuacamoleRdpConnection.ps1
@@ -1,91 +1,116 @@
 Function New-GuacamoleRdpConnection {
 <#
 .SYNOPSIS
-    Createas a new Guacamole-Connections
+    Createas a new Guacamole-RDP-Connection.
 .DESCRIPTION
     
 .EXAMPLE
-    PS C:\> New-GuacamoleConnection
+    PS C:\> $Password = Convertto-Securestring -String "2manySecrets!" -AsPlainText -force
+    PS C:\> New-GuacamoleRdpConnection -Hostname Server1 -Username Administrator -Password $Password
+    This Command creates a new RDP-Connection-Setting with the given values. Username and Password are
+    the names with which Guacamole will log in to the Remote Computer. The Connection will automatically
+    be named after the Computer.
+.EXAMPLE
+    PS C:\> $Password = Convertto-Securestring -String "2manySecrets!" -AsPlainText -force
+    PS C:\> New-GuacamoleRdpConnection -Hostname Server1 -Username Administrator -Password $Password -OtherProperties @{"initial-program"="C:\windows\Notepad.exe"}
+    This Command will set the inital programm. You can add further Properties inside the hashtable, but 
+    the key-value-Pairs have to be separated by a semi-colon, eg. @{"sftp-username"="john";"sftp-password"="TopSecret!"}
 .NOTES
     Author: Holger Voges
     Version: 1.0 
     Date: 2021-11-13
 #>    
     param(
-        [Parameter(Mandatory,
-                   ValueFromPipelineByPropertyName)]
-        [Alias('Name','Connection')]                   
-        [string]$Connectionname,
-
+        # The name of the target-Host as Computername or IP
         [Parameter(Mandatory,
                    ValueFromPipelineByPropertyName)]
         [Alias('ComputerName','Host','Computer')]
         [string]$Hostname,
 
+        # The Connectionname, defaults to the Computer-Name
         [Parameter(ValueFromPipelineByPropertyName)]
+        [Alias('Name','Connection')]                   
+        [string]$Connectionname = $Hostname,
+
+        # The RDP-Accountname 
+        [Parameter(Mandatory,
+                   ValueFromPipelineByPropertyName)]
         [string]$UserName,
 
-        [Parameter(ValueFromPipelineByPropertyName)]
-        [string]$Password,
+        # The RDP-Accountnames Password
+        [Parameter(Mandatory,
+                   ValueFromPipelineByPropertyName)]
+        [securestring]$UserPassword,
 
+        # The RDP-Accountnames Domain. Use Localhost if you want to authenticate locally. 
         [Parameter(ValueFromPipelineByPropertyName)]
-        [string]$Domain,
+        [string]$Domain = 'LocalHost',
 
+        # The Timezone for the RDP-Session
         [Parameter(ValueFromPipelineByPropertyName)]
 #       [ValidateSet($Timezone)]
-        [string]$TimeZone = 'Europe/Berlin',
+        [GuacamoleTimezones]$TimeZone = 'Europe_Berlin',
 
+        # The Keyboard-Layout
         [Parameter(ValueFromPipelineByPropertyName)]
         [string]$ServerLayout = 'de-de-qwertz',
 
+        # Skip the Certificate-Warning if the https-Certificate is not trusted
         [Parameter(ValueFromPipelineByPropertyName)]
         [string]$IgnoreCertificateWarning = 'true',
         
+        # The defaul securicty Mode for the Connection. NLA is default. 
         [Parameter(ValueFromPipelineByPropertyName)]
         [ValidateSet('NLA','any','RDP','TLS','vmconnect')]
         [string]$SecurityMode = 'NLA',
 
+        # 
         [Parameter(ValueFromPipelineByPropertyName)]
         [ValidateSet('display-update','reconnect')]
         [string]$ResizeMethod = 'display-update',
 
+        # The RDP-Port. Default is 3389
         [Parameter(ValueFromPipelineByPropertyName)]
         [ValidateRange(1,65535)]
-        [Int]$RdpPort = 3389,
+        [int]$RdpPort = 3389,
 
-        [Parameter(ValueFromPipelineByPropertyName)]
-        [string]$RdpGatewayHostName,
-
+        # The Useraccount for validation against the RDP-Gateway
         [Parameter(ValueFromPipelineByPropertyName)]
         [string]$RdpGatewayUserName,
 
+        # The RDP-Gateway-Users Password as Secure String
         [Parameter(ValueFromPipelineByPropertyName)]
-        [string]$RdpGatewayUserPassword,
+        [securestring]$RdpGatewayUserPassword,
 
+        # The RDP-Gateway-Users Domain
         [Parameter(ValueFromPipelineByPropertyName)]
         [string]$RdpGatewayUserDomain,
 
+        # The Port on which the RDP-Gateway is listening (normally 443)
         [Parameter(ValueFromPipelineByPropertyName)]
         [ValidateRange(1,65535)]
         [Int]$RdpGatewayPort,
 
+        # The maximum number of parallel RDP-Connections possible to this Host.
         [Parameter(ValueFromPipelineByPropertyName)]
         [Int]$MaximumConnections = 1,
 
+        # The maximum Connections per User. This value cannot be greater than MaximumConnections
         [Parameter(ValueFromPipelineByPropertyName)]
         [ValidateScript({ If ( $_ -gt $MaximumConnections ){ Throw "The number of connections per user cannot be greater then the number of maximum Connections"; $true }  })]
         [Int]$MaximumConnectionsPerUser = 1,
+
+        # Otherproperties can hold any configuration parameter. OtherProperties must be a 
+        # hashtable in the form @{"parametername"="parametervalue"}. The Parameter-Name has to 
+        # be in Qutation-Marks.
+        [hashtable]$OtherProperties,
 
         $AuthToken = $GuacAuthToken
     )
 
     Process {
         $EndPoint = '{0}/api/session/data/{1}/connections?token={2}' -f $AuthToken.HostUrl,$AuthToken.Datasource,$AuthToken.AuthToken
-
-        If (-not ($ConnectionName))    
-        {
-            $ConnectionName = $Hostname
-        }
+        Write-Verbose $Endpoint
 
         $Connection = [ordered]@{
             parentIdentifier = "ROOT"
@@ -95,42 +120,24 @@ Function New-GuacamoleRdpConnection {
             }
             attributes = [ordered]@{
             }
-            }
-
-        Switch ($PSBoundParameters.Keys) {
-            "Connectionname"            { $Connection.parameters["Connectionname"]          = $PSBoundParameters["Connectionname"] }
-            "hostname"                  { $Connection.parameters["hostname"]                = $PSBoundParameters["hostname"] }
-            "username"                  { $Connection.parameters["username"]                = $PSBoundParameters["connectionName"] }
-            "password"                  { $Connection.parameters["password"]                = $PSBoundParameters["Password"] }
-            "domain"                    { $Connection.parameters["domain"]                  = $PSBoundParameters["domain"] }
-            "timezone"                  { $Connection.parameters["timezone"]                = $PSBoundParameters["timezone"] }        
-            "SecurityMode"              { $Connection.parameters["security"]                = $PSBoundParameters["SecurityMode"] }
-            "IgnoreCertificateWarning"  { $Connection.parameters["ignore-cert"]             = $PSBoundParameters["IgnoreCertificateWarning"] }
-            "ServerLayout"              { $Connection.parameters["server-layout"]           = $PSBoundParameters["ServerLayout"] }
-            "ResizeMethod"              { $Connection.parameters["resize-method"]           = $PSBoundParameters["ResizeMethod"] }
-            "RdpPort"                   { $Connection.parameters["Port"]                    = $PSBoundParameters["RdpPort"] }
-            "RdpGatewayHostName"        { $Connection.parameters["gateway-hostname"]        = $PSBoundParameters["RdpGatewayHostname"] }
-            "RdpGatewayUserName"        { $Connection.parameters["gateway-username"]        = $PSBoundParameters["RdpGatewayUserName"] }
-            "RdpGatewayUserPassword"    { $Connection.parameters["gateway-password"]        = $PSBoundParameters["RdpGatewayUserPassword"] }
-            "RdpGatewayUserDomain"      { $Connection.parameters["gateway-domain"]          = $PSBoundParameters["RdpGatewayUserdomain"] }                        
-            "RdpGatewayPort"            { $Connection.parameters["gateway-port"]            = $PSBoundParameters["RdpGatewayPort"] }
-            "MaximumConnections"        { $Connection.attributes["max-connections"]         = $PSBoundParameters["MaximumConnections"] }
-            "MaximumConnectionsPerUser" { $Connection.attributes["max-connections-per-user"]= $PSBoundParameters["MaximumConnectionsPerUser"] }
         }
-        $Connectionparameters = $Connection | ConvertTo-Json    
-        # $Connectionparameters
-        Write-Verbose $Endpoint
-        # $Connections = Invoke-WebRequest -Uri $EndPoint -Method Post -ContentType 'application/json' -Body $Connectionparameters
-    }
-}
 
-<#    $Connectionparameters = @"
-    {
+        If ( $UserPassword )
+        {   
+            $UserPasswordUnprotected = ([pscredential]::new('User',$UserPassword)).GetNetworkCredential().Password
+        }
+        If ( $RdpGatewayUserPassword )
+        {   
+            $RdpGatewayPasswordUnprotected = ([pscredential]::new('User',$RdpGatewayUserPassword)).GetNetworkCredential().Password
+        }
+
+        $Connectionparameters = @"
+{
     "parentIdentifier": "ROOT",
     "name": "$ConnectionName",
     "protocol": "rdp",
     "parameters": {
-        "port": "3389",
+        "port": "$RdpPort",
         "read-only": "",
         "swap-red-blue": "",
         "cursor": "",
@@ -147,10 +154,10 @@ Function New-GuacamoleRdpConnection {
         "sftp-port": "",
         "sftp-server-alive-interval": "",
         "enable-audio": "",
-        "security": "",
+        "security": "$SecurityMode",
         "disable-auth": "",
         "ignore-cert": "$IgnoreCertificateWarning",
-        "gateway-port": "",
+        "gateway-port": $RdpGatewayPort,
         "server-layout": "$ServerLayout",
         "timezone": "$TimeZone",
         "console": "",
@@ -175,13 +182,13 @@ Function New-GuacamoleRdpConnection {
         "disable-glyph-caching": "",
         "preconnection-id": "",
         "hostname": "$Hostname",
-        "Connectionname": "$Connectionname",
-        "password": "$Password",
+        "username": "$UserName",
+        "password": "$UserPasswordUnprotected",
         "domain": "$Domain",
-        "gateway-hostname": "",
-        "gateway-Connectionname": "",
-        "gateway-password": "",
-        "gateway-domain": "",
+        "gateway-hostname": "$RdpGatewayHostName",
+        "gateway-username": "$RdpGatewayUserName",
+        "gateway-password": "$RdpGatewayPasswordUnprotected",
+        "gateway-domain": "$RdpGatewayUserDomain",
         "initial-program": "",
         "client-name": "",
         "printer-name": "",
@@ -197,14 +204,14 @@ Function New-GuacamoleRdpConnection {
         "recording-name": "",
         "sftp-hostname": "",
         "sftp-host-key": "",
-        "sftp-Connectionname": "",
+        "sftp-username": "",
         "sftp-password": "",
         "sftp-private-key": "",
         "sftp-passphrase": "",
         "sftp-root-directory": "",
         "sftp-directory": ""
     },
-    "parameters": {
+    "attributes": {
         "max-connections": "",
         "max-connections-per-Connection": "",
         "weight": "",
@@ -213,5 +220,23 @@ Function New-GuacamoleRdpConnection {
         "guacd-encryption": "",
         "guacd-hostname": ""
     }
+}
+"@ 
+
+        if ( $OtherProperties ) {
+            Foreach ( $Property in $OtherProperties.GetEnumerator() )
+            {
+                $SearchValue = '"{0}": ""' -f $Property.key
+                $ReplacementValue = '"{0}": "{1}"' -f $Property.key,$Property.Value
+                $Connectionparameters = $Connectionparameters -replace $Searchvalue,$ReplacementValue
+            }
+        }
+
+        Try {
+            $Connections = Invoke-WebRequest -Uri $EndPoint -Method Post -ContentType 'application/json' -Body $Connectionparameters
+        }
+        Catch {
+            Throw $_
+        }
     }
-"@ #>
+}
