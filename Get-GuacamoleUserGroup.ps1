4685ebae9b4a9e8a834a697f60e4069ccde6769b
Holger Voges <holger.voges@netz-weise.de>
2022-02-18 17:12:07 +0100

    Get-GuacamoleUsergroups URL angepasst

--- Get-GuacamoleUserGroup.ps1
+++ Get-GuacamoleUserGroup.ps1
@@ -17,12 +17,19 @@ Function Get-GuacamoleUserGroup {
                    ValueFromPipelineByPropertyName)]
         [string]$GroupName,
 
+        # List all Attributes as Object Properties, even empty ones. If you use this parameter, you 
+        # may break the Pipeline-Functionality.
+        [Switch]$ShowEmptyAttributes,
+
+        # Returns the Raw JSON-String
+        [Switch]$Raw,
+
         $AuthToken = $GuacAuthToken
     )
 
     Process {
         if ( $GroupName ) {
-            $EndPoint = '{0}/api/session/data/{1}/users/{3}/userGroups?token={2}' -f $AuthToken.HostUrl,$AuthToken.datasource,$AuthToken.authToken,$GroupName
+            $EndPoint = '{0}/api/session/data/{1}/userGroups/{3}?token={2}' -f $AuthToken.HostUrl,$AuthToken.datasource,$AuthToken.authToken,$GroupName
         }
         Else {
             $EndPoint = '{0}/api/session/data/{1}/userGroups?token={2}' -f $AuthToken.HostUrl,$AuthToken.datasource,$AuthToken.authToken
@@ -31,10 +38,23 @@ Function Get-GuacamoleUserGroup {
         Write-Verbose $Endpoint
         Try {
             $WebResponse = Invoke-WebRequest -UseBasicParsing -Uri $EndPoint -ErrorAction Stop
-            $WebResponse | ConvertFrom-Json
+            $GroupList = $WebResponse.Content | ConvertFrom-Json
         }
         Catch {
             Throw $_.Exception.Message
         }
+
+        if ( $Raw ) { 
+            $WebResponse.Content
+         }
+        elseif ( $GroupList.identifier ) {
+            Get-GuacamoleAttributes -Object $GroupList -ShowEmptyAttributes:$ShowEmptyAttributes
+        }
+        Else {
+            Foreach ( $Property in $GroupList.psobject.properties.Name ) {
+                Get-GuacamoleAttributes -Object $GroupList.$Property -ShowEmptyAttributes:$ShowEmptyAttributes
+            }        
+        }
+
     }
 }
