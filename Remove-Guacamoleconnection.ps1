fc5c1b153250685598f554a59b9d33b3c2886787
Holger Voges <holger.voges@netz-weise.de>
2022-02-19 22:54:52 +0100

    Remove-GuacamoleConnection: Added Pipeline-Functionality

--- Remove-Guacamoleconnection.ps1
+++ Remove-Guacamoleconnection.ps1
@@ -1,26 +1,53 @@
 Function Remove-GuacamoleConnection {
 <#
 .SYNOPSIS
-    Returns an individual or all Guacamole-Connections
+    Removes one or several Connections
 .DESCRIPTION
-    Returns an individual or all Guacamole-Connections
+    Removes one or several Connections. This Cmdlet makes no difference
+    between the types of connections and can use pipeline-Input
 .EXAMPLE
-    PS C:\> Get-GuacamoleConnection -ConnectionID PC12
+    PS C:\> Remove-GuacamoleConnection -ConnectionID 47
     Returns the Connection-Settings for the Connection PC12
 .NOTES
     Author: Holger Voges
     Version: 1.0 
-    Date: 2021-11-13
+    Date: 202-02-18
 #>    
     param(
-        [Parameter(Mandatory)]
-        $AuthToken = $GuacAuthToken,
+        # The ID of the Connection you want to remove
+        [Parameter(Mandatory,
+                   ValueFromPipeline,
+                   ValueFromPipelineByPropertyName,
+                   ParameterSetName="ById")]
+        [string]$ConnectionID,
 
-        [string]$ConnectionID
+        # The Name of the Connection you want to remove
+        [Parameter(Mandatory,
+                   ValueFromPipeline,
+                   ValueFromPipelineByPropertyName,
+                   ParameterSetName="ByName")]
+        [string]$ConnectionName,
+
+        [Parameter(DontShow)]
+        $AuthToken = $GuacAuthToken        
     )
 
-    $EndPoint = '{0}/api/session/data/{1}/connections/{3}?token={2}' -f $AuthToken.HostUrl,$AuthToken.datasource,$AuthToken.authToken,$ConnectionID
+    Begin {
+        If ( $PSCmdlet.ParameterSetName -eq "ByName" )
+        {
+            $connections = Get-GuacamoleConnection
+        }
+    }
 
-    Write-Verbose $Endpoint
-    $Connections = Invoke-WebRequest -Uri $EndPoint -Method Delete
+    Process {
+        Try {
+            $EndPoint = '{0}/api/session/data/{1}/connections/{3}?token={2}' -f $AuthToken.HostUrl,$AuthToken.datasource,$AuthToken.authToken,$ConnectionID
+        }
+        Catch {
+            Throw $_
+        }
+
+        Write-Verbose $Endpoint
+        $Connections = Invoke-WebRequest -Uri $EndPoint -Method Delete
+    }
 }
