b65dc1f78b6f110a68515f17268a1e554e8e558c
Holger Voges <holger.voges@netz-weise.de>
2022-02-21 19:30:59 +0100

    Added KeyboardLayout-Enum and Lookup-Table

--- init.ps1
+++ init.ps1
@@ -1,7 +1,3 @@
-New-Variable -Scope Script -name GuacamoleTimezones -Option Constant -Value "Europe/Amsterdam","Europe/Andorra","Europe/Athens","Europe/Belfast","Europe/Belgrade","Europe/Berlin","Europe/Bratislava","Europe/Brussels","Europe/Bucharest","Europe/Budapest","Europe/Busingen","Europe/Chisinau","Europe/Copenhagen","Europe/Dublin","Europe/Gibraltar","Europe/Guernsey","Europe/Helsinki","Europe/IsleOfMan","Europe/Isanbul","Europe/Jersey"
-$GuacamoleTimezones
-
-
 Enum GuacamoleTimezones {
     Europe_Amsterdam
     Europe_Andorra
@@ -25,4 +21,44 @@ Enum GuacamoleTimezones {
     Europe_Jersey
 }
 
-[GuacamoleTimezones]::Europe_Amsterdam
+Enum KeyboardLayout {
+    Danish
+    Brazilian_Portugese
+    English_UK
+    English_US
+    French
+    French_Belgian
+    French_Swiss
+    German
+    German_Swiss
+    Hungarian
+    Italian
+    Japanese
+    Norwegian
+    Spanish
+    Spanish_Latin_American
+    Swedish
+    Turkish
+    Unicode
+}
+
+$Script:keyboardCodes = @{
+    Danish = 'da-dk-qwerty'
+    Brazilian_Portugese = 'pt-br-qwerty'
+    English_UK = 'en-gb-qwerty'
+    English_US = 'en-us-qwerty'
+    French = 'fr-fr-azerty'
+    French_Belgian = 'fr-be-azerty'
+    French_Swiss = 'fr-ch-qwertz'
+    German = 'de-de-qwertz'
+    German_Swiss = 'de-ch-qwertz'
+    Hungarian = 'hu-hu-qwertz'
+    Italian = 'it-it-qwerty'
+    Japanese = 'ja-jp-qwerty'
+    Norwegian = 'no-no-qwerty'
+    Spanish = 'es-es-qwerty'
+    Spanish_Latin_American = 'es-latam-qwerty'
+    Swedish = 'sv-se-qwerty'
+    Turkish = 'tr-tr-qwerty'  
+    Unicode = 'failsafe'
+}
